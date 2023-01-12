03d63db96672d7b364b49072fcc71b18680950a7
stellaTHORSTEINSSON <62014926+stellaTHORSTEINSSON@users.noreply.github.com>
2023-01-12 14:54:13 +0100

    Adding hello world program.

---
+++ TP1/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::string name;
+    std::cout << "What's your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello " << name << "!" << std::endl;
+    return 0;
+}
